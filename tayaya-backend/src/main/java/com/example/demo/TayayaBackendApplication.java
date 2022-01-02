b43cd835f3e99b24a50878540df60339cada2f78
Mehdi Ben Dhiab <mehdibendhiab5@gmail.com>
2022-01-02 12:30:32 +0100

    created spring boot application

---
+++ tayaya-backend/src/main/java/com/example/demo/TayayaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TayayaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TayayaBackendApplication.class, args);
+	}
+
+}
